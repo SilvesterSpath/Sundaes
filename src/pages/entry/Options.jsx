39b251c2f15d0196452fd280c307297c8d87c013
SilvesterSpath <spath.sz@gmail.com>
2023-01-03 11:07:24 +0100

    ScoopOption

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,3 +1,27 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import ScoopOption from './ScoopOption';
+import Row from 'react-bootstrap/Row';
+
 export default function Options({ optionType }) {
-  return <div />;
+  const [items, setItems] = useState([]);
+
+  // optionType is 'scoops' or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => setItems(res.data))
+      .catch((error) => {
+        // TODO: handle error response
+      });
+  }, [optionType]);
+
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+
+  const optionItems = items.map((i) => (
+    <ItemComponent key={i.name} name={i.name} imagePath={i.imagePath} />
+  ));
+
+  return <Row>{optionItems}</Row>;
 }
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -2,11 +2,11 @@ import { render, screen } from '@testing-library/react';
 
 import Options from '../Options';
 
-test('displays image for each scoop option from server', () => {
+test('displays image for each scoop option from server', async () => {
   render(<Options optionType='scoops' />);
 
   // find images
-  const scoopImages = screen.getAllByRole('img', { name: /scoop$/i });
+  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
 
   expect(scoopImages).toHaveLength(2);
 
