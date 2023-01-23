b3dc82b9eaa8b12d37eab3431a40c475c9f8abdd
SilvesterSpath <spath.sz@gmail.com>
2023-01-23 06:55:06 +0100

    OrderEntry.test.js with wrapper

--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,10 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { OrderDetailsProvider } from '../../../contexts/OrderDetails';
 import Options from '../Options';
 
 test('updatescoop subtotal when scoops change', async () => {
   const user = userEvent.setup();
-  render(<Options optionType='scoops' />);
+  render(<Options optionType='scoops' />, { wrapper: OrderDetailsProvider });
 
   // make sure total starts out at $0.00
   const scoopsSubtotal = screen.getByText('Scoops total: $', { exact: false });
